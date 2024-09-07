2f0e89cffe29b9699be937d12dc5556e1974ef9c
codethefeel <yogeshsolankim76@gmail.com>
2024-09-07 19:29:50 +0530

    change error code

--- src/firebase/setup.js
+++ src/firebase/setup.js
@@ -1,10 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDHaKhqid9unWnN3eZ_JZ_z6HMxUEuM2Z8",
